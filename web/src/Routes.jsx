fa754db328f0854a83460b7f20b23805986394ff
Aashish <aashish.panigrahi@carboledger.com>
2024-10-14 13:40:09 +0530

    Toggle theme passed to the theme provider based on the global state

--- web/src/Routes.jsx
+++ web/src/Routes.jsx
@@ -1,12 +1,16 @@
 import { Router, Route, Set } from '@redwoodjs/router'
-
 import AppLayout from './layouts/AppLayout/AppLayout'
 import { CssBaseline, ThemeProvider } from '@mui/material'
 import LightTheme from './themes/lightTheme'
+import useStore from './state/store'
+import DarkTheme from './themes/darkTheme'
 
 const Routes = () => {
+    // ACCESING GLOBAL STORE
+    const { isDarkMode } = useStore() || {}
+
     return (
-        <ThemeProvider theme={LightTheme}>
+        <ThemeProvider theme={isDarkMode ? DarkTheme : LightTheme}>
             <CssBaseline />
             <Router>
                 <Set wrap={AppLayout}>
--- web/src/layouts/AppLayout/AppLayoutContainer.jsx
+++ web/src/layouts/AppLayout/AppLayoutContainer.jsx
@@ -1,10 +1,11 @@
 import { Box, styled } from '@mui/material'
 
-const AppLayoutContainer = styled(Box)(() => ({
+const AppLayoutContainer = styled(Box)(({ theme }) => ({
     '&.MuiBox-root': {
         maxWidth: '1200px',
         marginLeft: 'auto',
         marginRight: 'auto',
+        padding: theme.spacing(2),
         boxSizing: 'border-box',
     },
 }))

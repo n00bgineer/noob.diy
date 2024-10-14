eeda42f58d6cc3f5e831cb4fc0a41bb636f54374
Aashish <aashish.panigrahi@carboledger.com>
2024-10-14 13:38:46 +0530

    Completing the header component

--- web/src/components/Header/Header.jsx
+++ web/src/components/Header/Header.jsx
@@ -1,13 +1,31 @@
-import { Box, Link, Typography } from '@mui/material'
+import {
+    Box,
+    IconButton,
+    Link,
+    Menu,
+    MenuItem,
+    useMediaQuery,
+} from '@mui/material'
 import HeaderContainer from './HeaderContainer'
 import { Link as RouterLink, routes, useLocation } from '@redwoodjs/router'
 import AppLogo from 'src/assets/logo.png'
+import useStore from 'src/state/store'
+import { DarkMode, LightMode, Menu as MenuIcon } from '@mui/icons-material'
+import { useState } from 'react'
 
 const Header = () => {
     // SETTING HOOKS
     const { pathname } = useLocation()
+    const isMobileViewport = useMediaQuery('(min-width:900px)')
+
+    // ACCESING GLOBAL STATES
+    const { isDarkMode, toggleDarkMode } = useStore() || {}
+
+    // SETTING LOCAL STATES
+    const [anchorEl, setAnchorEl] = useState(null)
 
     // SETTING LOCAL VARIABLES
+    const open = Boolean(anchorEl)
     const links = [
         {
             route: routes.home(),
@@ -15,6 +33,7 @@ const Header = () => {
             className: `${
                 pathname === routes.home() ? 'link link-selected' : 'link'
             }`,
+            selected: pathname === routes.home(),
         },
         {
             route: routes.feed(),
@@ -22,29 +41,86 @@ const Header = () => {
             className: `${
                 pathname === routes.feed() ? 'link link-selected' : 'link'
             }`,
+            selected: pathname === routes.feed(),
         },
     ]
+
+    // METHODS
+    /**
+     * @name handleOpenNavigationMenu
+     * @description METHOD TO OPEN NAVIGATION MENU
+     * @param {*} event EVENT OBJECT
+     * @returns {undefined} undefined
+     */
+    const handleOpenNavigationMenu = (event) => {
+        setAnchorEl(event.currentTarget)
+    }
+
+    /**
+     * @name handleCloseNavigationMenu
+     * @description METHOD TO CLOSE NAVIGATION MENU
+     * @param {*} event EVENT OBJECT
+     * @returns {undefined} undefined
+     */
+    const handleCloseNavigationMenu = () => {
+        setAnchorEl(null)
+    }
 
     return (
         <HeaderContainer>
             <img src={AppLogo} alt="App logo" className="appLogo" />
-            <Box>
-                {links.map((link) => {
-                    const { route, name, className } = link || {}
-                    console.log(link)
-
-                    return (
-                        <Link
-                            key={name}
-                            component={RouterLink}
-                            to={route}
-                            underline="none"
-                            className={className}
+            <Box className="cta-container">
+                <IconButton onClick={() => toggleDarkMode()}>
+                    {isDarkMode ? <DarkMode /> : <LightMode />}
+                </IconButton>
+                {!isMobileViewport ? (
+                    <>
+                        <IconButton onClick={handleOpenNavigationMenu}>
+                            <MenuIcon />
+                        </IconButton>
+                        <Menu
+                            anchorEl={anchorEl}
+                            open={open}
+                            onClose={handleCloseNavigationMenu}
+                            MenuListProps={{
+                                'aria-labelledby': 'navigation-menu-item',
+                            }}
                         >
-                            {name}
-                        </Link>
-                    )
-                })}
+                            {links.map((link) => {
+                                const { route, name, selected } = link || {}
+
+                                return (
+                                    <MenuItem
+                                        key={name}
+                                        component={RouterLink}
+                                        to={route}
+                                        underline="none"
+                                        onClick={handleCloseNavigationMenu}
+                                        selected={selected}
+                                    >
+                                        {name}
+                                    </MenuItem>
+                                )
+                            })}
+                        </Menu>
+                    </>
+                ) : (
+                    links.map((link) => {
+                        const { route, name, className } = link || {}
+
+                        return (
+                            <Link
+                                key={name}
+                                component={RouterLink}
+                                to={route}
+                                underline="none"
+                                className={className}
+                            >
+                                {name}
+                            </Link>
+                        )
+                    })
+                )}
             </Box>
         </HeaderContainer>
     )
--- web/src/components/Header/HeaderContainer.jsx
+++ web/src/components/Header/HeaderContainer.jsx
@@ -10,6 +10,27 @@ const HeaderContainer = styled(Box)(({ theme }) => ({
         '&> .appLogo': {
             width: theme.spacing(8),
         },
+        '&> .cta-container': {
+            display: 'flex',
+            justifyContent: 'flex-start',
+            alignItems: 'center',
+            gap: theme.spacing(1),
+
+            '&> .link': {
+                padding: theme.spacing(2),
+                borderRadius: '50%',
+            },
+
+            '&> .link-selected': {
+                paddingTop: theme.spacing(1),
+                paddingLeft: theme.spacing(2),
+                paddingRight: theme.spacing(2),
+                paddingBottom: theme.spacing(1),
+                borderRadius: theme.spacing(4),
+                background: theme.palette.primary.main,
+                color: theme.palette.primary.contrastText,
+            },
+        },
     },
 }))
 

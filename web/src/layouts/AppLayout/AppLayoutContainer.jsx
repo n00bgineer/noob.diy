94d8e4039a89b27b244c0b16ea9899f0dd95813a
Aashish <aashish.panigrahi@carboledger.com>
2024-10-14 11:41:07 +0530

    Minor: Adding box-sizing

--- web/src/layouts/AppLayout/AppLayoutContainer.jsx
+++ web/src/layouts/AppLayout/AppLayoutContainer.jsx
@@ -1,11 +1,12 @@
 import { Box, styled } from '@mui/material'
 
 const AppLayoutContainer = styled(Box)(() => ({
-  '&.MuiBox-root': {
-    maxWidth: '1200px',
-    marginLeft: 'auto',
-    marginRight: 'auto',
-  },
+    '&.MuiBox-root': {
+        maxWidth: '1200px',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        boxSizing: 'border-box',
+    },
 }))
 
 export default AppLayoutContainer

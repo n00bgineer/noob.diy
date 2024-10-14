dc5ae292a6a2638a58eef6dd2a90b9ade4a29a4f
Aashish <aashish.panigrahi@carboledger.com>
2024-10-14 10:21:25 +0530

    Adding new pages

--- web/src/Routes.jsx
+++ web/src/Routes.jsx
@@ -1,17 +1,14 @@
-// In this file, all Page components from 'src/pages` are auto-imported. Nested
-// directories are supported, and should be uppercase. Each subdirectory will be
-// prepended onto the component name.
-//
-// Examples:
-//
-// 'src/pages/HomePage/HomePage.js'         -> HomePage
-// 'src/pages/Admin/BooksPage/BooksPage.js' -> AdminBooksPage
+import { Router, Route, Set } from '@redwoodjs/router'
 
-import { Router, Route } from '@redwoodjs/router'
+import AppLayout from './layouts/AppLayout/AppLayout'
 
 const Routes = () => {
   return (
     <Router>
+      <Set wrap={AppLayout}>
+        <Route path="/blog" page={BlogPage} name="blog" />
+        <Route path="/" page={HomePage} name="home" />
+      </Set>
       <Route notfound page={NotFoundPage} />
     </Router>
   )

beb6a48014a7c69d4c3a726ca990037a875611e8
Aashish <aashish.panigrahi@carboledger.com>
2024-10-15 22:17:20 +0530

    Minor: Replacing store w/ persistent store

--- web/src/state/store.js
+++ web/src/state/store.js
@@ -1,8 +1,16 @@
 import { create } from 'zustand'
+import { persist } from 'zustand/middleware'
 
-const useStore = create((set) => ({
-    isDarkMode: false,
-    toggleDarkMode: () => set((state) => ({ isDarkMode: !state.isDarkMode })),
-}))
-
-export default useStore
+export const usePersistentStore = create(
+    persist(
+        (set) => ({
+            isDarkMode: false,
+            toggleDarkMode: () =>
+                set((state) => ({ isDarkMode: !state.isDarkMode })),
+        }),
+        {
+            name: 'noob-local-storage',
+            getStorage: () => localStorage,
+        }
+    )
+)

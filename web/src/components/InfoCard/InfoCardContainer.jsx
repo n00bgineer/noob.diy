b3c874d9709c51894d0928af8de062b320f2e6ce
Aashish <aashish.panigrahi@carboledger.com>
2024-10-16 07:16:47 +0530

    Updating styles for responsiveness

--- web/src/components/InfoCard/InfoCardContainer.jsx
+++ web/src/components/InfoCard/InfoCardContainer.jsx
@@ -29,16 +29,16 @@ const InfoCardContainer = styled(Card)(({ theme }) => ({
             alignItems: 'center',
         },
         '&.card-w1': {
-            flex: 1,
-            width: '25%',
+            flex: 'unset',
+            width: '100%',
         },
         '&.card-w2': {
-            flex: 2,
-            width: '50%',
+            flex: 'unset',
+            width: '100%',
         },
         '&.card-w3': {
-            flex: 3,
-            width: '75%',
+            flex: 'unset',
+            width: '100%',
         },
         '&.card-wfull': {
             width: '100%',
@@ -69,6 +69,78 @@ const InfoCardContainer = styled(Card)(({ theme }) => ({
             textTransform: 'unset',
             fontSize: '12px',
             boxShadow: 'unset',
+        },
+    },
+    [theme.breakpoints.up('md')]: {
+        '&.MuiCard-root.card': {
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'flex-start',
+            borderRadius: theme.spacing(3),
+            border: `1px solid ${theme.palette.divider}`,
+
+            '&.card-title': {
+                marginBottom: theme.spacing(3),
+            },
+            '&.card-small': {
+                height: '100px',
+            },
+            '&.card-normal': {
+                height: '200px',
+            },
+            '&.card-large': {
+                height: '300px',
+            },
+            '&.card-padding': {
+                padding: theme.spacing(3),
+            },
+            '&.card-center': {
+                justifyContent: 'center',
+                alignItems: 'center',
+            },
+            '&.card-w1': {
+                flex: 1,
+                width: '25%',
+            },
+            '&.card-w2': {
+                flex: 2,
+                width: '50%',
+            },
+            '&.card-w3': {
+                flex: 3,
+                width: '75%',
+            },
+            '&.card-wfull': {
+                width: '100%',
+            },
+            '& .card-logo': {
+                width: '60px',
+                height: '60px',
+                borderRadius: '50%',
+                marginBottom: '0.75rem',
+                display: 'block',
+            },
+            '& .card-title': {
+                fontWeight: '500',
+                color: theme.palette.primary.main,
+                marginBottom: '0.25rem',
+                display: 'block',
+            },
+            '& .card-description': {
+                fontWeight: '400',
+                color: theme.palette.primary[700],
+                marginBottom: '0.75rem',
+                display: 'block',
+                whiteSpace: 'pre-wrap',
+            },
+            '& .card-button': {
+                borderRadius: theme.spacing(1),
+                padding: '4px 8px',
+                textTransform: 'unset',
+                fontSize: '12px',
+                boxShadow: 'unset',
+            },
         },
     },
 }))
--- web/src/pages/HomePage/HomePageContainer.jsx
+++ web/src/pages/HomePage/HomePageContainer.jsx
@@ -19,10 +19,9 @@ const HomePageContainer = styled(Box)(({ theme }) => ({
             display: 'flex',
             alignItems: 'center',
             justifyContent: 'flex-start',
-            gap: theme.spacing(6),
+            gap: theme.spacing(3),
             width: '100%',
             flexWrap: 'wrap',
-            marginBottom: theme.spacing(1),
         },
 
         '& .full-image': {
@@ -43,6 +42,53 @@ const HomePageContainer = styled(Box)(({ theme }) => ({
             left: '16px',
             position: 'absolute',
             fontWeight: 'bold',
+        },
+    },
+
+    [theme.breakpoints.up('md')]: {
+        '&.MuiBox-root': {
+            marginTop: theme.spacing(8),
+            marginBottom: theme.spacing(5),
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'stretch',
+            alignItems: 'flex-start',
+            gap: theme.spacing(3),
+
+            '& .name': {
+                fontWeight: '900',
+            },
+
+            // CARD STYLES
+            '& .card-horizontal-container': {
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'flex-start',
+                gap: theme.spacing(6),
+                width: '100%',
+                flexWrap: 'wrap',
+                marginBottom: theme.spacing(1),
+            },
+
+            '& .full-image': {
+                width: '100%',
+                height: '100%',
+                objectFit: 'cover',
+            },
+
+            '& .map-location': {
+                borderRadius: theme.spacing(1),
+                padding: '4px 8px',
+                textTransform: 'unset',
+                fontSize: '12px',
+                boxShadow: 'unset',
+                background: 'rgba(255, 255, 255, 0.6)',
+                color: 'rgba(0,0,0)',
+                bottom: '16px',
+                left: '16px',
+                position: 'absolute',
+                fontWeight: 'bold',
+            },
         },
     },
 }))

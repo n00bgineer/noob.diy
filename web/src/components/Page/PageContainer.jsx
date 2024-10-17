a65fd0c9ca1ffa4bf5872c3f5eb39d90304b5716
n00bgineer <n00bgineer@protonmail.com>
2024-10-18 03:19:40 +0530

    Updating posts page

--- web/src/components/Page/PageContainer.jsx
+++ web/src/components/Page/PageContainer.jsx
@@ -33,6 +33,21 @@ const PageContainer = styled(Box)(({ theme }) => ({
         '& .page-body-non-success-image': {
             width: '250px',
         },
+
+        '& .link': {
+            padding: theme.spacing(2),
+            borderRadius: '50%',
+        },
+
+        '& .link-selected': {
+            paddingTop: theme.spacing(1),
+            paddingLeft: theme.spacing(2),
+            paddingRight: theme.spacing(2),
+            paddingBottom: theme.spacing(1),
+            borderRadius: theme.spacing(4),
+            color: theme.palette.primary.main,
+            border: `1px solid ${theme.palette.primary.main}`,
+        },
     },
 
     [theme.breakpoints.down('md')]: {
--- web/src/pages/Post/PostsPage/PostsPage.jsx
+++ web/src/pages/Post/PostsPage/PostsPage.jsx
@@ -1,7 +1,27 @@
+import { useState } from 'react'
+import { Link } from '@mui/material'
+import Page from 'src/components/Page/Page'
 import PostsCell from 'src/components/Post/PostsCell'
+import { Link as RouterLink, routes } from '@redwoodjs/router'
 
 const PostsPage = () => {
-    return <PostsCell />
+    return (
+        <Page
+            headerTitle="Feed"
+            headerChildren={
+                <Link
+                    component={RouterLink}
+                    to={routes.newPost()}
+                    underline="none"
+                    className="link link-selected"
+                >
+                    Post
+                </Link>
+            }
+        >
+            <PostsCell />
+        </Page>
+    )
 }
 
 export default PostsPage
